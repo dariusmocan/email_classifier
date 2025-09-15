04d2f88406944aa59979047723242c841c7fb79d
dariusmocan <dariusmocan2003@gmail.com>
2025-09-15 14:47:38 +0300

    implementing Multinomial Naive Bayes classifier using CountVectorizer for word counts

--- src/main.py
+++ src/main.py
@@ -1,17 +1,43 @@
 import numpy as np
 import pandas as pd
 import sklearn as sk
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
 from data import DataLoader
 
 
-# Usage example:
+nb = MultinomialNB()
+ 
+
 loader = DataLoader()
 easy_ham, hard_ham, spam = loader.load_emails("data/archive.zip")
 
+all_emails = easy_ham + hard_ham + spam
+labels = ['easy_ham'] * len(easy_ham) + ['hard_ham'] * len(hard_ham) + ['spam'] * len(spam)
+
+vectorizer = CountVectorizer(lowercase = True)
+X = vectorizer.fit_transform(all_emails)
+
+X_train, X_test, y_train, y_test = train_test_split(X,labels,test_size=0.2,random_state=42)
+
+nb = MultinomialNB()
+nb.fit(X_train,y_train)
+
+y_pred = nb.predict(X_test)
+
+print("Accuracy :", accuracy_score(y_test, y_pred))
+
+print("Precision :", precision_score(y_test, y_pred, average="macro"))
+
+print("Recall :", recall_score(y_test, y_pred, average="macro"))
+
+print("f1_score :", f1_score(y_test, y_pred, average="macro"))
+
+# print("Vocabulary:", vectorizer.vocabulary_)
 
 
 
-    # class NaiveBayes:
-    #     def __init__(self):
         
 
--- src/tests.py
+++ src/tests.py
@@ -2,32 +2,32 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 ex = ["Hello dear friend", "Friend can you give me some money", "What you up today"]
 
-vectorizer = CountVectorizer()
+vectorizer = CountVectorizer(lowercase=True)
 X = vectorizer.fit_transform(ex)
 
-print("Sparse matrix:")
-print(X)
+# print("Sparse matrix:")
+# print(X)
 
-print("\nDense matrix (cu numerele efectiv):")
+# print("\nDense matrix (cu numerele efectiv):")
+# print(X.toarray())
+
+# print("\nCuvintele (vocabulary):")
+# print(vectorizer.get_feature_names_out())
+
+# print("\nCorespondența cuvânt - număr:")
+# for word, idx in vectorizer.vocabulary_.items():
+#     print(f"{word}: index {idx}")
+
+# print("\nCuvintele sunt sortate alfabetic:")
+# print(vectorizer.get_feature_names_out())
+
+# print("\nStatistici globale (pentru toate textele):")
+# total_counts = X.toarray().sum(axis=0)  # sumează pe toate textele
+# feature_names = vectorizer.get_feature_names_out()
+# for word, count in zip(feature_names, total_counts):
+#     print(f"'{word}': {count} apariții în total")
+
+print("Vocabulary:", vectorizer.vocabulary_)
+
 print(X.toarray())
 
-print("\nCuvintele (vocabulary):")
-print(vectorizer.get_feature_names_out())
-
-print("\nCorespondența cuvânt - număr:")
-for word, idx in vectorizer.vocabulary_.items():
-    print(f"{word}: index {idx}")
-
-print("\nCuvintele sunt sortate alfabetic:")
-print(vectorizer.get_feature_names_out())
-
-print("\nStatistici globale (pentru toate textele):")
-total_counts = X.toarray().sum(axis=0)  # sumează pe toate textele
-feature_names = vectorizer.get_feature_names_out()
-for word, count in zip(feature_names, total_counts):
-    print(f"'{word}': {count} apariții în total")
-
-print("\nPentru Naive Bayes ai nevoie de:")
-print("- Frecvența cuvintelor PER CLASĂ (spam vs ham)")
-print("- Nu doar frecvența totală")
-print("- De ex: 'money' apare în 80% din spam, dar doar 5% din ham")
