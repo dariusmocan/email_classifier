f4503c0fb3b6b57d748ecde4a0939218f2f94aab
dariusmocan <dariusmocan2003@gmail.com>
2025-09-15 18:46:56 +0300

    classifying custom email

--- src/main.py
+++ src/main.py
@@ -5,11 +5,22 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
+import tkinter as tk
+from tkinter import filedialog
 from data import DataLoader
 
 
-nb = MultinomialNB()
- 
+def load_email():
+    tk.Tk().withdraw()
+    file_path = filedialog.askopenfilename(title="Select an email", 
+                                           filetypes=[("Text files", "*.txt"), ("Email files", "*.eml") , ("All files", "*.")])
+    if file_path:
+        with open(file_path,"r",encoding="utf-8") as f:
+            return f.read()
+    else:
+        return None
+
+
 
 loader = DataLoader()
 easy_ham, hard_ham, spam = loader.load_emails("data/archive.zip")
@@ -20,7 +31,7 @@ labels = ['easy_ham'] * len(easy_ham) + ['hard_ham'] * len(hard_ham) + ['spam'] 
 vectorizer = CountVectorizer(lowercase = True)
 X = vectorizer.fit_transform(all_emails)
 
-X_train, X_test, y_train, y_test = train_test_split(X,labels,test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,labels,test_size=0.2,random_state=42, stratify=labels)
 
 nb = MultinomialNB()
 nb.fit(X_train,y_train)
@@ -36,6 +47,14 @@ print("Recall :", recall_score(y_test, y_pred, average="macro"))
 print("f1_score :", f1_score(y_test, y_pred, average="macro"))
 
 # print("Vocabulary:", vectorizer.vocabulary_)
+
+email_text = load_email()
+if email_text:
+    X_new = vectorizer.transform([email_text])
+    prediction = nb.predict(X_new)
+    print("Prediction for new email :", prediction)
+else:
+    print("No Email selected!")
 
 
 
